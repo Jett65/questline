435e6717249a744cb4d8432775121386bf8e0dec
KadenR <kadenreynolds05@gmail.com>
2024-01-09 00:52:28 -0700

    added get all api route for tasks

--- APIs/gameAPI/handler_tasks.go
+++ APIs/gameAPI/handler_tasks.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 
 	_ "github.com/Jett65/questline/APIs/gameAPI/internal/database"
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/google/uuid"
+	"github.com/google/uuid"
 )
 
 func (apiCfg *apiconfig) handlerCreateTask(c *fiber.Ctx) error {
@@ -13,24 +13,38 @@ func (apiCfg *apiconfig) handlerCreateTask(c *fiber.Ctx) error {
 }
 
 func (apiCfg *apiconfig) handlerGetAllTasks(c *fiber.Ctx) error {
-	// tasks, err := apiCfg.DB.GetTask(c.Context())
-	// if err != nil {
-	// 	return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Fetch Tasks:::: %e", err))
-	// }
-	return nil
+	tasks, err := apiCfg.DB.GetTask(c.Context())
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Fetch Tasks:::: %e", err))
+	}
+
+	payload, err := databasetasksToTasks(tasks)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Convert Tasks:::: %e", err))
+	}
+
+	fmt.Println(fiber.StatusAccepted)
+
+	return c.JSON(payload)
 }
 
 func (apiCfg *apiconfig) handlerGetTaskById(c *fiber.Ctx) error {
-	// task_id, err := uuid.Parse(c.Params("id"))
-	// if err != nil {
-	// 	 return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Not a valid ID:::: %e", err))
-	// }
+	task_id, err := uuid.Parse(c.Params("id"))
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Not a valid ID:::: %e", err))
+	}
 
-	// task, err := apiCfg.DB.GetTaskById(c.Context(), task_id)
-	// if err != nil {
-	// 	return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Fetch Task:::: %e", err))
-	// }
-	return nil
+	task, err := apiCfg.DB.GetTaskById(c.Context(), task_id)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Fetch Task:::: %e", err))
+	}
+
+	payload, err := databaseTaskToTask(task)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("::::Failed to Convert Task:::: %e", err))
+	}
+
+	return c.JSON(payload)
 }
 
 func (apiCfg *apiconfig) handlerUpdateTaskById(c *fiber.Ctx) error {
