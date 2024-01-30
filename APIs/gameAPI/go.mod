0c21becd05a8f8adbc86f84da23344834002c3df
Jett65 <jettc505@gmail.com>
2024-01-30 12:13:39 -0700

    added cors

--- APIs/gameAPI/go.mod
+++ APIs/gameAPI/go.mod
@@ -4,9 +4,11 @@ go 1.21.1
 
 require (
 	github.com/gofiber/fiber/v2 v2.51.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.4.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.7.0
 )
 
 require (
--- APIs/gameAPI/utils.go
+++ APIs/gameAPI/utils.go
@@ -2,7 +2,60 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
+
+var secret_key = []byte(os.Getenv("SECRET_KEY"))
+
+func genJWT(key []byte, climes jwt.MapClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, climes)
+
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string, key []byte) error {
+    token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+        return key, nil
+    })
+    if err != nil {
+        return err
+    }
+
+    if !token.Valid {
+        return fmt.Errorf("invalid token")
+    }
+
+    return nil
+}
+
+// middlewhere to authorize the user
+func isAuth(c *fiber.Ctx) error {
+    authHeader := string(c.Request().Header.Peek("Authorization"))
+    
+    splitAuthHeader := strings.Split(authHeader, " ")
+    if len(splitAuthHeader) != 2 || splitAuthHeader[0] != "Bearer" {
+        return fiber.NewError(400, "::::Header could not be parsed")
+    }
+
+    tokenString := splitAuthHeader[1]
+
+    err := verifyToken(tokenString, secret_key)
+    if err != nil {
+        return fiber.NewError(400, "::::User not authorized")
+    }
+
+    return c.Next()
+}
 
 func ParseNullString(nullString *sql.NullString) string {
 	return nullString.String
@@ -22,9 +75,3 @@ func ToNullString(str string) sql.NullString {
 	}
 }
 
-// func handleErr(c *fiber.Ctx, message string, err error) error {
-// 	if err == nil {
-// 		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintln("::::"+message+"::::", "%e"), err.Error())
-// 	}
-// 	return nil
-// }
