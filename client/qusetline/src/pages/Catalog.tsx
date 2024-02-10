c7789bc6ef5bb60d2718b4ac1b1c27befee2f479
KadenR <kadenreynolds05@gmail.com>
2024-02-09 18:15:27 -0700

    added catalog games

--- client/qusetline/src/pages/Catalog.tsx
+++ client/qusetline/src/pages/Catalog.tsx
@@ -2,32 +2,43 @@ import React from "react"
 import { useEffect, useState } from "react"
 
 function Catalog() {
-  const [games, setGames] = useState([]);
+
+  const [games, setGames] = useState<Array<{
+    id: string,
+    name: string,
+    image_url: string,
+    description: string
+  }>>([])
 
   useEffect(() => {
-    // fetchGames()
+    fetchGames()
   }, [])
-
-  async function fetchGames () {
+  
+  async function fetchGames() {
     let api = 'http://localhost:3006/api/v1'
-
-    const response = await fetch(`${api}/catalog_game`)
-    const result = await response.json()
-
-    console.log(result)
-    setGames(result)
+    
+    try {
+      const response = await fetch(`${api}/catalog_game`)
+      const result = await response.json()
+      console.log(result)
+      setGames(result)
+    } catch (error) {
+      console.error(error)
+    }
   }
+  console.log(games)
+
 
   return(
+    
   <div>
     <h1>Games Catalog!</h1>
     <h3>Check out our Games</h3>
 
     <div className="catalogGameDiv">
-      {/* {games.map((game) => (
-        <div key={game.id}></div>
-      ))} */}
-      <button onClick={fetchGames}>Click me For games</button>
+      {games.map((game) => (
+        <div key={game.id}>{game.name}</div>
+      ))}
     </div>
   </div>
   )
