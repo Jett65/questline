f8da60859756cf41bb0e86fae5a56d35a9a1883c
Jett65 <jettc505@gmail.com>
2024-05-19 16:45:16 -0600

    renamed gameAPI

---
+++ APIs/qusetlineAPI/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/Jett65/questline/APIs/gameAPI/internal/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+type apiconfig struct {
+	DB *database.Queries
+}
+
+func main() {
+	godotenv.Load(".env")
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database")
+	}
+
+	apiCfg := apiconfig{
+		DB: database.New(conn),
+	}
+
+	app := fiber.New()
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+    v1.Use(cors.New((cors.Config{
+        AllowOrigins: "http://localhost:5173",
+    })))
+
+	v1.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendString("It's Up")
+	})
+
+	// Catalog Game Routes
+	v1.Post("/catalog_game", apiCfg.handlerCreateCatalogGame)
+	v1.Get("/catalog_game", apiCfg.handlerGetAllCatalogGames)
+	v1.Get("/catalog_game/:id", apiCfg.handlerGetCatalogGameById)
+	v1.Put("/catalog_game/:id", apiCfg.handlerUpdateCatalogGame)
+	v1.Delete("/catalog_game/:id", apiCfg.handlerDeleteCatalogGameById)
+
+	// Tasks Routes
+	v1.Post("/tasks", apiCfg.handlerCreateTask)
+	v1.Get("/tasks", apiCfg.handlerGetAllTasks)
+	v1.Get("/tasks/:id", apiCfg.handlerGetTaskById)
+	v1.Put("/tasks/:id", apiCfg.handlerUpdateTaskById)
+	v1.Delete("/tasks/:id", apiCfg.handlerDeleteTaskById)
+	v1.Get("/:gameid/tasks", apiCfg.handlerGetTasksByCatalogGameId)
+
+    // User Routes
+    v1.Post("/create_account", apiCfg.handlerCreateUser)
+    v1.Post("/login", apiCfg.handlerlogin)
+    v1.Get("/is_logged_in", isAuth, apiCfg.isLoggedIn)
+    v1.Delete("/users/:id", apiCfg.handlerDeleteUser)
+
+	log.Fatal(app.Listen(":3006"))
+}
---
+++ APIs/qusetlineAPI/models.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"github.com/Jett65/questline/APIs/gameAPI/internal/database"
+	"github.com/google/uuid"
+)
+
+// CatalogGames
+type CatalogGame struct {
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ImageURL    string    `json:"image_url"`
+}
+
+func databaseCatalogGameToCatalogGame(dbCatalogGame database.CatalogGame) (CatalogGame, error) {
+	return CatalogGame{
+		ID:          dbCatalogGame.ID,
+		Name:        dbCatalogGame.Name,
+		Description: dbCatalogGame.Description.String,
+		ImageURL:    dbCatalogGame.Imageurl.String,
+	}, nil
+}
+
+func databaseCatalogGamesToCatalogGames(dbCatalogGames []database.CatalogGame) ([]CatalogGame, error) {
+	games := []CatalogGame{}
+	for _, game := range dbCatalogGames {
+		dbGame, err := databaseCatalogGameToCatalogGame(game)
+		if err != nil {
+			return games, err
+		}
+
+		games = append(games, dbGame)
+	}
+
+	return games, nil
+}
+
+func catalogGameToDatabeseCatalogGame(catalogGmae *CatalogGame) (database.CatalogGame, error) {
+	return database.CatalogGame{
+		ID:          catalogGmae.ID,
+		Name:        catalogGmae.Name,
+		Description: ToNullString(catalogGmae.Description),
+		Imageurl:    ToNullString(catalogGmae.ImageURL),
+	}, nil
+}
+
+// Tasks
+type Task struct {
+	ID          uuid.UUID `json:"id"`
+	Description string    `json:"description"`
+	Game_id     uuid.UUID `json:"game_id"`
+}
+
+func databaseTaskToTask(dbTask database.Task) (Task, error) {
+	return Task{
+		ID:          dbTask.ID,
+		Description: dbTask.Description.String,
+		Game_id:     dbTask.GameID,
+	}, nil
+}
+
+func databasetasksToTasks(dbTasks []database.Task) ([]Task, error) {
+	tasks := []Task{}
+	for _, task := range dbTasks {
+		dbGame, err := databaseTaskToTask(task)
+		if err != nil {
+			return tasks, err
+		}
+
+		tasks = append(tasks, dbGame)
+	}
+
+	return tasks, nil
+}
+
+func taskToDatabaseTask(task Task) (database.Task, error) {
+	return database.Task{
+		ID:          task.ID,
+		Description: ToNullString(task.Description),
+		GameID:      task.Game_id,
+	}, nil
+}
+
+// Users
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
+}
+
+//TODO: Create email validation
+
+func databaseUserToUser(dbUser database.User) (User, error) {
+	return User{
+		ID:       dbUser.ID,
+		Username: dbUser.Username,
+		Email:    dbUser.Email,
+		Password: dbUser.Password,
+	}, nil
+}
+
+func databaseUsersToUsers(dbUsers []database.User) ([]User, error) {
+	users := []User{}
+
+	for _, user := range dbUsers {
+		dbUser, err := databaseUserToUser(user)
+		if err != nil {
+			return users, err
+		}
+
+		users = append(users, dbUser)
+	}
+
+	return users, nil
+}
